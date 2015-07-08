511bd04dde49aef623000b2f63dc028d81c94b6a
Kevin Hou <kevin.hou@sjsu.edu>
2015-07-07 21:46:32 -0700

    updated problem solutions

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,6 +1,31 @@
 /**
- * Problem Statement:
+ * Problem Statement: https://leetcode.com/problems/reverse-integer/
  * Created by kevin
  */
 public class ReverseInteger {
+    // this problem is also solvable by casting to string and swapping first and last until center
+    public int reverse(int x) {
+        if (x == 0) return 0;
+        long input = x;
+        if (input > Integer.MAX_VALUE || input < Integer.MIN_VALUE) {
+            return 0;
+        }
+        boolean wasNeg = false;
+        long reversed = 0;
+        if (input < 0) {
+            input = Long.parseLong(("" + x).substring(1));
+            wasNeg = true;
+        }
+        while (input != 0) {
+            reversed = reversed * 10 + input % 10;
+            input /= 10;
+        }
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+        if (wasNeg) {
+            return Integer.parseInt("-" + (int) reversed);
+        }
+        return (int) reversed;
+    }
 }
--- RomanToInt.java
+++ RomanToInt.java
@@ -1,6 +1,35 @@
+import java.util.Hashtable;
+
 /**
- * Problem Statement:
+ * Problem Statement: https://leetcode.com/problems/roman-to-integer/
  * Created by kevin
  */
 public class RomanToInt {
+    public int romanToInt(String s) {
+        Hashtable<Character, Integer> romans = new Hashtable<>();
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+        int integer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i != s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) { // next letter is equal
+                integer += romans.get(s.charAt(i)) + romans.get(s.charAt(i + 1));
+                i++;
+            } else { // not equal
+                if (i == s.length() - 1) {
+                    integer += romans.get(s.charAt(i));
+                } else if (romans.get(s.charAt(i + 1)) > romans.get(s.charAt(i))) {
+                    integer += (romans.get(s.charAt(i + 1)) - romans.get(s.charAt(i)));
+                    i++;
+                } else {
+                    integer += romans.get(s.charAt(i));
+                }
+            }
+        }
+        return integer;
+    }
 }
