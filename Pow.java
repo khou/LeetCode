171f85a31473b8ca7572f0f9d47e7d64887e1ebd
Kevin Hou <kevin.hou@sjsu.edu>
2015-07-07 21:48:55 -0700

    added Pow problem solution

---
+++ Pow.java
@@ -0,0 +1,10 @@
+/**
+ * Problem Statement: https://leetcode.com/problems/powx-n/
+ * Created by kevin
+ */
+public class Pow {
+    public double myPow(double x, int n) {
+        // TODO: solve with bit shift
+        return Math.pow(x,n);
+    }
+}
